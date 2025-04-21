ae7f320cce336fa1a0d147d87a276edccc8b4a1d
Zcy <zcy_chao1024@163.com>
2025-04-21 22:52:25 +0800

    play idle && run

---
+++ cocos/assets/Scripts/Player.ts
@@ -0,0 +1,44 @@
+import {_decorator, Component, Animation, input, Input, EventKeyboard, KeyCode, Vec3} from 'cc';
+
+const {ccclass, property} = _decorator;
+
+@ccclass('Player')
+export class Player extends Component {
+    @property({type: Animation})
+    anim: Animation = null;
+
+    private _moveDir = 1;
+    private _speed = 0;
+
+    onLoad() {
+        input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    update(deltaTime: number) {
+        const pos = this.node.getPosition();
+        this.node.setPosition(pos.x + this._moveDir * this._speed * deltaTime, pos.y, 0);
+    }
+
+    onDestroy() {
+        input.off(Input.EventType.KEY_DOWN, this.onKeyDown, this);
+        input.off(Input.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    onKeyDown(event: EventKeyboard) {
+        if (event.keyCode !== KeyCode.KEY_A && event.keyCode !== KeyCode.KEY_D)
+            return;
+        this._moveDir = event.keyCode === KeyCode.KEY_A ? -1 : 1;
+        this.node.scale = new Vec3(this._moveDir, 1, 1);
+        this._speed = 100;
+        this.anim.play("PlayerRun");
+    }
+
+    onKeyUp(event: EventKeyboard) {
+        if (event.keyCode !== KeyCode.KEY_A && event.keyCode !== KeyCode.KEY_D)
+            return;
+        this._speed = 0;
+        this.anim.play("PlayerIdle");
+    }
+}
+
