a4cc162b1ce4df63f209c2d25034eeedee4ca2d7
Zcy <zcy_chao1024@163.com>
2025-04-21 21:50:46 +0800

    enemy auto move

--- cocos/assets/Scripts/Enemy.ts
+++ cocos/assets/Scripts/Enemy.ts
@@ -34,10 +34,22 @@ export class Enemy extends Component {
     private _color = new Color(255, 255, 255, 255);
     private _defaultColor = new Color(255, 255, 255, 255);
     private _scale = new Vec3(0.6, 0.6, 1);
+    private _targetX = 0;
+    private _speed = 0;
+    private _moveDir = 1;
 
     onLoad() {
         this._spine = this.node.getComponent(sp.Skeleton);
         this.node.on(Node.EventType.TOUCH_START, this.onTouchDown, this);
+        this.randomTargetPos();
+    }
+
+    update(deltaTime: number) {
+        const pos = this.node.getPosition();
+        while (Math.abs(pos.x - this._targetX) < this._speed / 100)
+            this.randomTargetPos();
+        this._moveDir = this._targetX - pos.x > 0 ? 1 : -1;
+        this.node.setPosition(pos.x + this._moveDir * this._speed * deltaTime, pos.y, pos.z);
     }
 
     async onTouchDown() {
@@ -59,14 +71,14 @@ export class Enemy extends Component {
     onNormal() {
         this._spine.setAnimation(0, "run", true);
         this._spine.color = this._defaultColor;
-        this._scale.x = 0.6;
+        this._scale.x = 0.6 * this._moveDir;
         this.node.scale = this._scale;
     }
 
     onHit() {
         this._spine.setAnimation(0, "hit", false);
         this._spine.color = this._color;
-        this._scale.x = 0.6 - 0.2 * Math.random();
+        this._scale.x = (0.6 - 0.2 * Math.random()) * this._moveDir;
         tween(this.node).to(this._time, {scale: this._scale}).to(this._time * 0.5, {scale: this._scale}).start();
     }
 
@@ -102,6 +114,16 @@ export class Enemy extends Component {
         const pos = this.node.getPosition();
         node.setPosition(pos.x - 40 + 80 * Math.random(), pos.y + 150, 0);
         node.getComponent(dmgCtrl).init(damage, showType);
+    }
+
+    randomTargetPos() {
+        const pos = this.node.getPosition();
+        this._targetX = Math.random() * 500 * (Math.floor(Math.random() * 2) === 0 ? -1 : 1);
+        // 100 - 600
+        this._speed = 100;
+        this._moveDir = this._targetX - pos.x > 0 ? 1 : -1;
+        this._scale.x =  this._moveDir === 1 ? 0.6 : -0.6;
+        this.node.scale = this._scale;
     }
 }
 
