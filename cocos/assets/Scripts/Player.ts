a1aaff56700bf54cdffc2a325d529fccbc15e40a
Zcy <zcy_chao1024@163.com>
2025-04-22 10:31:53 +0800

    add attack

--- cocos/assets/Scripts/Player.ts
+++ cocos/assets/Scripts/Player.ts
@@ -1,4 +1,5 @@
-import {_decorator, Component, Animation, input, Input, EventKeyboard, KeyCode, Vec3} from 'cc';
+import {_decorator, Component, Animation, input, Input, EventKeyboard, KeyCode, Vec3, AudioClip} from 'cc';
+import {AudioManager} from "db://assets/Scripts/AudioManager";
 
 const {ccclass, property} = _decorator;
 
@@ -6,9 +7,13 @@ const {ccclass, property} = _decorator;
 export class Player extends Component {
     @property({type: Animation})
     anim: Animation = null;
+    @property({type: AudioClip})
+    attackMusic: AudioClip = null;
 
     private _moveDir = 1;
     private _speed = 0;
+    private _attackTimer = 0;
+    private _isRunning = false;
 
     onLoad() {
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
@@ -16,6 +21,10 @@ export class Player extends Component {
     }
 
     update(deltaTime: number) {
+        if (this._attackTimer > 0) {
+            this._attackTimer -= deltaTime;
+            return;
+        }
         const pos = this.node.getPosition();
         let destX = pos.x + this._moveDir * this._speed * deltaTime;
         destX = Math.min(destX, 600);
@@ -29,12 +38,20 @@ export class Player extends Component {
     }
 
     onKeyDown(event: EventKeyboard) {
+        if (event.keyCode === KeyCode.KEY_J && this._attackTimer <= 0) {
+            this._attackTimer = 1;
+            this.anim.play("PlayerAttack");
+            this.scheduleOnce(() => this.anim.play(this._isRunning ? "PlayerRun" : "PlayerIdle"), this._attackTimer);
+            this.scheduleOnce(() => AudioManager.inst.playOneShot(this.attackMusic, 1), 0.3);
+            return;
+        }
         if (event.keyCode !== KeyCode.KEY_A && event.keyCode !== KeyCode.KEY_D)
             return;
         this._moveDir = event.keyCode === KeyCode.KEY_A ? -1 : 1;
         this.node.scale = new Vec3(this._moveDir, 1, 1);
         this._speed = 100;
         this.anim.play("PlayerRun");
+        this._isRunning = true;
     }
 
     onKeyUp(event: EventKeyboard) {
@@ -42,6 +59,7 @@ export class Player extends Component {
             return;
         this._speed = 0;
         this.anim.play("PlayerIdle");
+        this._isRunning = false;
     }
 }
 
