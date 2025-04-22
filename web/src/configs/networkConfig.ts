d35d21af644639683b802053a00e070bdaa759bb
Zcy <zcy_chao1024@163.com>
2025-04-22 19:56:59 +0800

    publish move && add some props to test

--- web/src/configs/networkConfig.ts
+++ web/src/configs/networkConfig.ts
@@ -22,6 +22,8 @@ const {networkConfig, useNetworkVariable, useNetworkVariables} = createNetworkCo
                 PackageID: "",
                 UpgradeCap: "",
                 Publisher: "",
+                PropsList: "",
+                Data: ""
             }
         }
     },
@@ -37,9 +39,11 @@ const {networkConfig, useNetworkVariable, useNetworkVariables} = createNetworkCo
                 UserTable: "0x9424be761fd89f5d2c6615536f1747f45ac6c3ad60e7f90f2f92744ad5d6752f"
             },
             FFD: {
-                PackageID: "",
-                UpgradeCap: "",
-                Publisher: "",
+                PackageID: "0x0e5a6f507dcd5fb27b8b61a36d5feb29cea3502b80f85e29012ce243c0acdc2a",
+                UpgradeCap: "0xa24da467f5965b8833fc6f94170440c93e0c65553dbe292f738fd80430b58b72",
+                Publisher: "0x9b695c08eae3d81fd8bea703ca5759b0cf94685528b81da4417461ac01d75b7d",
+                PropsList: "0xd7477c0049a4dbb62f39d24abc0343a5d05e78303c280ea570ed57ac8ed9ff87",
+                Data: "0x54865c472270a82b1a6da071a8a6058e744d6a83aa1c08a1a0f4b0fceecf28ce"
             }
         }
     }
