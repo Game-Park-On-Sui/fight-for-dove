92c9974e08db51ef2763168fc3c1bfb49177142d
Zcy <zcy_chao1024@163.com>
2025-04-25 09:18:22 +0800

    static export

--- web/src/components/ffdCard.tsx
+++ web/src/components/ffdCard.tsx
@@ -12,6 +12,13 @@ export default function FFDCard({info, editDropIds}: {
         editDropIds(info.fields.id.id, !dropped);
         setDropped(!dropped);
     }
+    const getShowKey = (key: string) => {
+        if (key === "criticalHitRate")
+            return "criticalRate";
+        if (key === "criticalDamage")
+            return "criticalDmg";
+        return key;
+    }
     const getRealValue = (type: string, value: number) => {
         value -= 1000;
         if (type === "blood")
@@ -34,7 +41,7 @@ export default function FFDCard({info, editDropIds}: {
                     <div className="absolute flex flex-col items-start -bottom-32 left-0 w-full pl-1 text-[#afb3b5] group-hover:bottom-2 transition-all duration-750 text-lg">
                         {
                             info.fields.effects.fields.contents.map((content, index) =>
-                                <span key={index}>{`${content.fields.key}: ${getRealValue(info.fields.props_type, Number(content.fields.value))}`}</span>)
+                                <span key={index}>{`${getShowKey(content.fields.key)}: ${getRealValue(info.fields.props_type, Number(content.fields.value))}`}</span>)
                         }
                     </div> ||
                     <div className="absolute w-full h-full left-0 top-0 text-[#041f4b] font-bold text-2xl text-center leading-36 transition-all duration-750">
